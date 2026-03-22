38def39abe9ffa26012a834bb4fd5b7bc0f61278
sumisorano <sumisorano@gmail.com>
2026-03-22 12:26:19 +0800

    Update hi-card.tsx

--- src/app/(home)/hi-card.tsx
+++ src/app/(home)/hi-card.tsx
@@ -8,13 +8,13 @@ function getGreeting() {
 	const hour = new Date().getHours()
 
 	if (hour >= 6 && hour < 12) {
-		return 'Good Morning'
+		return '晨光熹微，早上好'
 	} else if (hour >= 12 && hour < 18) {
-		return 'Good Afternoon'
+		return '午后悠长，下午好'
 	} else if (hour >= 18 && hour < 22) {
-		return 'Good Evening'
+		return '华灯初上，晚上好'
 	} else {
-		return 'Good Night'
+		return '夜色温柔，晚安'
 	}
 }
 
@@ -23,35 +23,47 @@ export default function HiCard() {
 	const { cardStyles, siteContent } = useConfigStore()
 	const greeting = getGreeting()
 	const styles = cardStyles.hiCard
-	const username = siteContent.meta.username || '泛舟的Blog'
+    
+	const username = siteContent.meta.username || '泛舟'
 
 	const x = styles.offsetX !== null ? center.x + styles.offsetX : center.x - styles.width / 2
 	const y = styles.offsetY !== null ? center.y + styles.offsetY : center.y - styles.height / 2
 
+	// 新年装饰开关：改成 false 就会自动隐藏装饰
+	const showLunarNewYear = true;
+
 	return (
 		<HomeDraggableLayer cardKey='hiCard' x={x} y={y} width={styles.width} height={styles.height}>
 			<Card order={styles.order} width={styles.width} height={styles.height} x={x} y={y} className='relative text-center max-sm:static max-sm:translate-0'>
-				{siteContent.enableChristmas && (
+				
+				{/* 🧧 农历新年装饰元素 */}
+				{showLunarNewYear && (
 					<>
+						{/* 左上角的装饰（比如红灯笼或梅花） */}
 						<img
-							src='/images/christmas/snow-1.webp'
-							alt='Christmas decoration'
+							src='/images/new-year/lantern-left.png'
+							alt='Lunar New Year decoration'
 							className='pointer-events-none absolute'
-							style={{ width: 180, left: -20, top: -25, opacity: 0.9 }}
+							style={{ width: 80, left: -10, top: -20, opacity: 0.95 }}
 						/>
+						{/* 右下角的装饰（比如小福字或鞭炮） */}
 						<img
-							src='/images/christmas/snow-2.webp'
-							alt='Christmas decoration'
+							src='/images/new-year/lantern-right.png'
+							alt='Lunar New Year decoration'
 							className='pointer-events-none absolute'
-							style={{ width: 160, bottom: -12, right: -8, opacity: 0.9 }}
+							style={{ width: 70, bottom: -10, right: -10, opacity: 0.95 }}
 						/>
 					</>
 				)}
+
 				<Link href='/live2d'>
-					<img src='/images/avatar.png' className='mx-auto rounded-full' style={{ width: 120, height: 120, boxShadow: ' 0 16px 32px -5px #E2D9CE' }} />
+					<img src='/images/avatar.png' className='mx-auto rounded-full object-cover' style={{ width: 120, height: 120, boxShadow: ' 0 16px 32px -5px #E2D9CE' }} alt="avatar" />
 				</Link>
-				<h1 className='font-averia mt-3 text-2xl'>
-					{greeting} <br /> I'm <span className='text-linear text-[32px]'>{username}</span> , Nice to <br /> meet you!
+				
+				<h1 className='mt-5 text-2xl tracking-widest'>
+					{greeting} <br /> 
+					我是 <span className='text-linear text-[32px] mx-1 font-bold'>{username}</span> <br /> 
+					<span className='text-lg text-gray-500 mt-2 block'>很高兴遇见你</span>
 				</h1>
 			</Card>
 		</HomeDraggableLayer>
